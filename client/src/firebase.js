f417a6af0806d09a888a7fa81f475a0e41148647
Basagoud <basagouda161@gmail.com>
2025-09-08 00:48:15 +0530

    added google oauth

---
+++ client/src/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "real-estate-3036c.firebaseapp.com",
+  projectId: "real-estate-3036c",
+  storageBucket: "real-estate-3036c.firebasestorage.app",
+  messagingSenderId: "850246932792",
+  appId: "1:850246932792:web:8e56542b4dc83d853509cd"
+};
+
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -6,6 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -27,9 +28,9 @@ export default function SignIn() {
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json', // ✅ FIXED
+          'Content-Type': 'application/json', 
         },
-        body: JSON.stringify(formData),       // ✅ SEND DATA
+        body: JSON.stringify(formData),       
       });
       const data = await res.json();
       console.log(data);
@@ -71,6 +72,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don’t have an account?</p>
