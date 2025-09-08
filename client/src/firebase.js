fda8dbd00862cb532619144ff31584ae4e38ccf3
Basagoud <basagouda161@gmail.com>
2025-09-08 11:13:30 +0530

    added image for profile

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "real-estate-3036c.firebaseapp.com",
   projectId: "real-estate-3036c",
-  storageBucket: "real-estate-3036c.firebasestorage.app",
+  storageBucket: "real-estate-3036c.appspot.com",
   messagingSenderId: "850246932792",
   appId: "1:850246932792:web:8e56542b4dc83d853509cd"
 };
