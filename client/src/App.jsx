9d163a4b5793b6f79db60349a030572af41909fc
Basagoud <basagouda161@gmail.com>
2025-09-08 23:46:33 +0530

    listing complete ui functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/signup" element={<Signup/>} />
         <Route element={<PrivateRoute/>}>
           <Route path="/profile" element={<Profile/>} />
+          <Route path='/create-listing' element={<CreateListing/>} />
         </Route>
       </Routes>
     </BrowserRouter>
